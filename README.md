3b3850f5033a2a250971729bb1cbc7059cdb1fd1
dcchuck <chuck.danielsson@gmail.com>
2018-05-01 23:03:22 -0400

    Basic implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+### Launch
+
+```bash
+npm install
+npm start
+```
+
+To replace the two applications, update the applications in the `main.js` file.
--- public/main.js
+++ public/main.js
@@ -8,8 +8,72 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+const applications = [
+	{
+		displayName: 'Yahoo Finance',
+		manifestUrl: 'http://localhost:3000/yahoo.json'
+	},
+	{
+		displayName: 'Apple',
+		manifestUrl: 'http://localhost:3000/apple.json'
+	}
+]
+
+function populateApplicationLinks() {
+	const linksParent = document.getElementById('application-list');
+	applications.forEach(application => {
+		const applicationLink = document.createElement('div');
+		applicationLink.innerText = application.displayName;
+		applicationLink.onclick = () => {
+			fin.desktop.Application.createFromManifest(application.manifestUrl,
+				(app) => app.run(),
+				(e) => console.log(e)
+			);
+		}
+		linksParent.appendChild(applicationLink);
+	})
+}
+
+function launchTwoAppsAndSplitMainScreen() {
+	launchTwoApps().then(
+		splitAppMainWindowsOnScreen
+	);
+}
+
+function launchTwoApps() {
+	return new Promise((res, rej) => {
+		fin.desktop.Application.createFromManifest(applications[0].manifestUrl, 
+			(app1) => {
+				app1.run(() => {
+					fin.desktop.Application.createFromManifest(applications[1].manifestUrl, (app2) => {
+						app2.run(res);
+					})
+				})
+			}
+		)
+	})
+}
+
+function splitAppMainWindowsOnScreen() {
+	const app1Window = fin.desktop.Window.wrap('yahoo', 'yahoo');
+	const app2Window = fin.desktop.Window.wrap('apple', 'apple');
+
+	fin.desktop.System.getMonitorInfo(monitorInfo => {
+		const mainMonitor = monitorInfo.primaryMonitor.availableRect;
+		console.log(mainMonitor)
+		app1Window.moveTo(mainMonitor.left, mainMonitor.top, 
+			() => app1Window.resizeTo((mainMonitor.right / 2), mainMonitor.bottom - mainMonitor.top)
+		)
+		app2Window.moveTo(mainMonitor.right / 2, mainMonitor.top, 
+			() => app2Window.resizeTo((mainMonitor.right / 2), mainMonitor.bottom - mainMonitor.top)
+		)
+	})
+}
+
 function init () {
     fin.desktop.System.getVersion(version => {
         console.log(version);
     });
+	populateApplicationLinks();
+	launchTwoAppsAndSplitMainScreen();
 }
